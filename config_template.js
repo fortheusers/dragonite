dd6a47849e44e3dc08546c092ea252a685201557
crc-32 <thomas.c.england@gmail.com>
2019-09-07 12:28:22 +0100

    Seperate config template, stops pull complaints

---
+++ config_template.js
@@ -0,0 +1,53 @@
+const config = {
+
+  discord: {
+    // your bot's token from the bot creation page
+    token: '', // it's a secret to everyone
+    commandPrefix: '.',
+
+    // All verification messages get sent here
+    packageVerification: {channel: '', guild: ''},
+    logging: {channel: '', guild: ''}
+  },
+
+  libget: {
+    // list of get repo url's to check via the bot
+    repos: ['', ''],
+      
+    // Max attempts at requesting something before giving up
+    toleranceMax: 3
+  },
+
+  gitlab: {
+    initOptions: {
+      token: ''
+    },
+
+    // the project to commit to upon submission approval (lines up with get repo)
+    projectIDs: {test: '12759473'}
+  },
+
+  github: {
+    initOptions: {
+        auth: ''
+    }
+  },
+
+  ssh: {
+    user: '',
+    server: '',
+    keyPath: ''
+  },
+
+  http: {
+    port: 457
+  },
+
+  database: {
+    pkgSalt: '',
+    banSalt: '',
+    metaSalt: ''
+  }
+};
+
+module.exports = config;
