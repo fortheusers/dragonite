b838bb17fb712f3ddec640ef54c124c5cfd8a4cf
Ash Logan <ash@heyquark.com>
2020-08-07 20:38:52 +1000

    submission: rewrite getGithubRelease logic

--- submission.js
+++ submission.js
@@ -19,57 +19,57 @@ setDiscord(msgid) {
 }
 
 async getGitHubRelease() {
-    var zipI = this.pkg.assets.indexOf(a => a.type === 'zip');
-    var binI = this.pkg.assets.indexOf(a => a.format !== 'base64' && (a.data.endsWith('.nro') || a.data.endsWith('.rpx') || a.data.endsWith('.elf')));
-    if (zipI === -1 && binI === -1) {
-        var gh = new GithubHelper();
-        var zipUrls = await gh.getReleases(this.pkg.info.url, '.zip');
-        var zipUrl;
-        if (zipUrls !== undefined && zipUrls.length > 1) {
-            zipUrl = zipUrls.find(a => a.name.includes(this.pkg.console));
-            if (zipUrl !== undefined)
-            {
-                zipUrl = zipUrl.browser_download_url;
-            }
-        }else if (zipUrls !== undefined && zipUrls.length > 0) {
-            zipUrl = zipUrls[0].browser_download_url;
-        }
-
-        var binUrl;
-        if (zipUrl === null || zipUrl === undefined || zipUrl === '') {
-            switch (this.pkg.console) {
-                case 'switch':
-                    binUrl = await gh.getRelease(this.pkg.info.url, '.nro');
-                    if (binUrl !== null && binUrl !== undefined) {
-                        this.pkg.assets.push({type: 'update', url: binUrl, dest: `/switch/${this.pkg.package}/${this.pkg.package}.nro`});
-                    }
-                    break;
-                case 'wiiu':
-                    binUrl = await gh.getRelease(this.pkg.info.url, '.rpx');
-                    var ext = '.rpx';
-                    if (binUrl === null || binUrl === undefined) {
-                        binUrl = await gh.getRelease(this.pkg.info.url, '.elf');
-                        ext = '.elf';
-                    }
-                    if (binUrl !== null && binUrl !== undefined) {
-                        this.pkg.assets.push({type: 'update', url: binUrl, dest: `/wiiu/apps${this.pkg.package}/${this.pkg.package}${ext}`});
-                    }
-                    break;
-                default:
-                    throw new Error(`The console ${this.pkg.console} is not supported by Dragonite auto-manifest but no predefined SD assets exist!`);
-                    return;
-            }
-
-        } else {
-            this.pkg.assets.push({type: 'zip', url: zipUrl, zip: [{path: '/**', dest:'/', type: 'update'}]});
-        }
-
-        if ((zipUrl === null || zipUrl == undefined) && (binUrl === null || binUrl === undefined)) {
-            throw new Error('Could not find binary-like assets on GitHub release!');
-            return;
-        }
+    const extensions = {
+        "switch": [".nro"],
+        "wiiu": [".rpx", ".elf"],
+    };
+    const consoleExtensions = extensions[this.pkg.console];
+    if (consoleExtensions === undefined) {
+        console.warn(`[Approval] WARNING: Unsupported console on package \
+                      ${this.pkg.package} - "${this.pkg.console}"?`)
+        consoleExtensions = [".elf"];
     }
 
+    const hasGoodAsset = this.pkg.assets.some(asset =>
+        asset.type === 'zip' ||
+        extensions[this.pkg.console].some(ext =>
+            asset.data.endsWith(ext)
+        )
+    );
+    if (hasGoodAsset) return;
+
+    var gh = new GithubHelper();
+    var releases = await gh.getReleases(this.pkg.info.url, '.zip');
+
+    let release = releases.find(release =>
+        release.name.includes(this.pkg.console)
+    )
+    if (release !== undefined && release.browser_download_url !== undefined) {
+        this.pkg.assets.push({
+            type: 'zip',
+            url: release.browser_download_url,
+            zip: [{path: '/**', dest:'/', type: 'update'}]
+        });
+        return;
+    }
+
+    let foundBinary = false;
+    for (const ext of consoleExtensions) {
+        let binRelease = await gh.getRelease(this.pkg.info.url, ext);
+        if (binRelease !== null && binRelease !== undefined) {
+            this.pkg.assets.push({
+                type: 'update',
+                url: binRelease,
+                dest: `/${this.pkg.console}/${this.pkg.package}/${this.pkg.package}${ext}`
+            });
+            foundBinary = true;
+            break;
+        }
+    };
+
+    if (!foundBinary) {
+        throw new Error(`Could not find ${this.pkg.console} zip or binary-like assets (${consoleExtensions.join()}) on GitHub release!`);
+    }
 } //getGitHubRelease()
 
 } //class Submission
